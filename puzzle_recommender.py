883bb18547f1bfe4ed0e3bb97ec42435df3db098
Aryan Srivastava <aryansrivastavaiit2024@gmail.com>
2025-10-27 17:34:46 +0530

    update in sites

--- puzzle_recommender.py
+++ puzzle_recommender.py
@@ -2,10 +2,10 @@ def get_puzzle_for_mood(mood):
     # Example mapping of moods to puzzle types
     mapping = {
         "happy": "https://gameofcrowns.sanish.me/",  # Queens-like (Star Battle)
-        "sad": "https://crossclimb-game.com/",       # Crossclimb
+        "sad": "https://www.crazygames.com/game/blockbuster-puzzle",       # Crossclimb
         "anxious": "https://www.tangogame.org/",     # Tango equivalent
-        "anger": "https://www.zipunlimitedgame.com",  # Zip style
-        "joy": "https://www.zipunlimitedgame.com",
+        "anger": "https://onlinegameweb.com/game/zip-game",  # Zip style
+        "joy": "https://onlinegameweb.com/game/zip-game",
 
 
     }
